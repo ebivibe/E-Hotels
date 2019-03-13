65c3c3bb212702fd8336c348fdde630a49f34402
ebivibe <ivanabe3@gmail.com>
2019-03-12 22:45:34 -0400

    fixed typos and added drops

--- SQL/Schemas.sql
+++ SQL/Schemas.sql
@@ -1,3 +1,4 @@
+DROP TABLE IF EXISTS HotelChain CASCADE;
 CREATE TABLE HotelChain (
  chain_id SERIAL PRIMARY KEY,
  chain_name VARCHAR (255) NOT NULL,
@@ -13,13 +14,14 @@ CREATE TABLE HotelChain (
  CONSTRAINT num_hotels CHECK (num_hotels >= 0)
 );
 
+DROP TABLE IF EXISTS ChainPhoneNumber CASCADE;
 CREATE TABLE ChainPhoneNumber(
     chain_id INT REFERENCES HotelChain(chain_id),
     phone_number VARCHAR(255) NOT NULL,
     PRIMARY KEY(chain_id, phone_number)
 );
 
-
+DROP TABLE IF EXISTS Hotel CASCADE;
 CREATE TABLE Hotel (
  hotel_id SERIAL PRIMARY KEY,
  chain_id INTEGER REFERENCES HotelChain(chain_id),
@@ -36,12 +38,14 @@ CREATE TABLE Hotel (
  CONSTRAINT categorybelow check (category <= 5)
 );
 
+DROP TABLE IF EXISTS HotelPhoneNumber CASCADE;
 CREATE TABLE HotelPhoneNumber(
     hotel_id INT REFERENCES Hotel(hotel_id),
     phone_number VARCHAR(255) NOT NULL,
     PRIMARY KEY(hotel_id, phone_number)
 );
 
+DROP TABLE IF EXISTS Room CASCADE;
 CREATE TABLE Room(
   room_id SERIAL PRIMARY KEY,
   room_number INT NOT NULL,
@@ -57,6 +61,7 @@ CREATE TABLE Room(
   CONSTRAINT capacity CHECK (capacity > 0)
 );
 
+DROP TABLE IF EXISTS Amenity CASCADE;
 CREATE TABLE Amenity(
     room_id INT REFERENCES Room(room_id),
     name VARCHAR(255) NOT NULL,
@@ -64,6 +69,7 @@ CREATE TABLE Amenity(
     PRIMARY KEY(room_id, name)
 );
 
+DROP TABLE IF EXISTS Employee CASCADE;
 CREATE TABLE Employee (
  SSN INT PRIMARY KEY,
  name VARCHAR (255) NOT NULL,
@@ -79,24 +85,27 @@ CREATE TABLE Employee (
  CONSTRAINT password CHECK (char_length(password) >= 5)
 );
 
+DROP TABLE IF EXISTS Manages CASCADE;
 CREATE TABLE Manages(
  SSN INT REFERENCES Employee(SSN),
  hotel_id INT REFERENCES Hotel(hotel_id)
 );
 
-
+DROP TABLE IF EXISTS Role CASCADE;
 CREATE TABLE Role(
  role_id SERIAL PRIMARY KEY,
  name VARCHAR(255) NOT NULL,
  description VARCHAR(255)
 );
 
+DROP TABLE IF EXISTS EmployeeRole CASCADE;
 CREATE TABLE EmployeeRole(
  employee_ssn INT REFERENCES Employee(SSN),
- role_id INT REFERENCES Role(role_id)
- PRIMARY KEY(employee_id, role_id)
+ role_id INT REFERENCES Role(role_id),
+ PRIMARY KEY(employee_ssn, role_id)
 );
 
+DROP TABLE IF EXISTS Customer CASCADE;
 CREATE TABLE Customer(
     SSN INT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
@@ -113,6 +122,7 @@ CREATE TABLE Customer(
      CONSTRAINT password CHECK (char_length(password) >= 5)
 );
 
+DROP TABLE IF EXISTS BookingRental CASCADE;
 CREATE TABLE BookingRental(
     booking_id SERIAL PRIMARY KEY,
     reservation_date TIMESTAMP NOT NULL,
@@ -125,6 +135,7 @@ CREATE TABLE BookingRental(
     CONSTRAINT booking_id CHECK (booking_id > 0)
 );
 
+DROP TABLE IF EXISTS Payment CASCADE;
 CREATE TABLE Payment(
     payment_id SERIAL PRIMARY KEY,
     booking_id INT REFERENCES BookingRental(booking_id),
@@ -138,6 +149,7 @@ CREATE TABLE Payment(
     CONSTRAINT card_number CHECK (card_number > 0)
 );
 
+DROP TABLE IF EXISTS Archive CASCADE;
 CREATE TABLE Archive (
     archive_id SERIAL PRIMARY KEY,
     room_number INT NOT NULL,
