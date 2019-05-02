d809e7bf8ab036ef9c0a2362bccbe3b1b3168e6a
IvanaE <33612255+ebivibe@users.noreply.github.com>
2019-05-01 23:51:59 -0400

    Update common.php

--- WebApp/helpers/common.php
+++ WebApp/helpers/common.php
@@ -1,4 +1,4 @@
 <?php
 error_reporting(E_ALL ^ E_WARNING);
-$dbconn2 = pg_connect("host=localhost port=5432 user=postgres password=T032m82347dha!"); 
+$dbconn2 = pg_connect("host=localhost port=5432 user=postgres password=PASSWORDHERE!"); 
 ?>
